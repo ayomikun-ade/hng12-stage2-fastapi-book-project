9c3f3787741875864774cd6c0aaaae550477a355
Ayomikun <ayoadeosun10@gmail.com>
2025-02-11 12:55:41 +0100

    fix: restored original code after ci workflow test

--- api/routes/books.py
+++ api/routes/books.py
@@ -48,14 +48,14 @@ async def get_books() -> OrderedDict[int, Book]:
     return db.get_books()
 
 
-# @router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
-# async def get_book(book_id: int):
-#     book = db.get_book(book_id)  
-#     if not book:
-#         return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={"detail": "Book not found"})
-#     return JSONResponse(
-#         status_code=status.HTTP_200_OK, content=book.model_dump()
-#     )
+@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+async def get_book(book_id: int):
+    book = db.get_book(book_id)  
+    if not book:
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={"detail": "Book not found"})
+    return JSONResponse(
+        status_code=status.HTTP_200_OK, content=book.model_dump()
+    )
 
 
 @router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
